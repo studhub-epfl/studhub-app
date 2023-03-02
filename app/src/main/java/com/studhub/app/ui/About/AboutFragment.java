64ee95fe2817ec03fc7dbb64f5f873650d57482a
rnasr <raymond.nasr@epfl.ch>
2023-03-02 15:08:07 +0100

    Fix CartFragment text not showing

--- app/src/main/java/com/studhub/app/ui/About/AboutFragment.java
+++ app/src/main/java/com/studhub/app/ui/About/AboutFragment.java
@@ -11,23 +11,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.studhub.app.R;
+import com.studhub.app.databinding.FragmentHomeBinding;
+import com.studhub.app.ui.Cart.CartViewModel;
 
 public class AboutFragment extends Fragment {
 
-    private AboutViewModel mViewModel;
+    private FragmentHomeBinding binding;
 
-    public static AboutFragment newInstance() {
-        return new AboutFragment();
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        AboutViewModel aboutViewModel =
+                new ViewModelProvider(this).get(AboutViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        aboutViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
-
-
 
 }
--- app/src/main/java/com/studhub/app/ui/Cart/CartFragment.java
+++ app/src/main/java/com/studhub/app/ui/Cart/CartFragment.java
@@ -4,24 +4,38 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.studhub.app.R;
+import com.studhub.app.databinding.FragmentHomeBinding;
+import com.studhub.app.ui.Home.HomeViewModel;
 
 public class CartFragment extends Fragment {
 
-    public static CartFragment newInstance() {
-        return new CartFragment();
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CartViewModel cartViewModel =
+                new ViewModelProvider(this).get(CartViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        cartViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
     }
 
-    private CartViewModel mViewModel;
-
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
     
 
