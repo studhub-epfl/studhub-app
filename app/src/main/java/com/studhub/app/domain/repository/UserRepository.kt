94dc52e59beb72adf414f0564578daa70bd392c0
Edouard Michelin <edouard.michelin15@gmail.com>
2023-03-07 18:50:05 +0100

    Refactored parameter name in updateUser

--- app/src/main/java/com/studhub/app/domain/repository/UserRepository.kt
+++ app/src/main/java/com/studhub/app/domain/repository/UserRepository.kt
@@ -7,6 +7,6 @@ import kotlinx.coroutines.flow.Flow
 interface UserRepository {
     suspend fun createUser(user: User): Flow<ApiResponse<Boolean>>
     suspend fun getUser(userId: Long): Flow<ApiResponse<User>>
-    suspend fun updateUser(userId: Long, user: User): Flow<ApiResponse<User>>
+    suspend fun updateUser(userId: Long, updatedUser: User): Flow<ApiResponse<User>>
     suspend fun removeUser(userId: Long): Flow<ApiResponse<Boolean>>
 }
