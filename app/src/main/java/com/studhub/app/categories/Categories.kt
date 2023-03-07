b07e2744d69404b0b2c498b83993dc3857487049
Edouard Michelin <edouard.michelin15@gmail.com>
2023-03-07 19:09:07 +0100

    First implementation of static categories

--- app/src/main/java/com/studhub/app/categories/Categories.kt
+++ app/src/main/java/com/studhub/app/categories/Categories.kt
@@ -1,6 +1,15 @@
 package com.studhub.app.categories
 
-enum class Categories(val description: String) {
+import com.studhub.app.domain.model.Category
+
+class Categories {
+    val Electronics: Category = Category(id = 1, name = "Electronics")
+    val SchoolsItem: Category = Category(id = 1, name = "School items")
+}
+
+
+
+enum class CategoriesEnum(val description: String) {
     ELECTRONICS("electronics"),
     SCHOOLSITEMS("school items"),
     INSTRUMENTS("music instruments"),
