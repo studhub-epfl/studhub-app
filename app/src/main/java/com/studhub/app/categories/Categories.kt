ab744ca642b3c5bd2a2dbfff275358197bcfa972
tepoorte <samuel.tepoorten@epfl.ch>
2023-03-08 18:51:32 +0100

    Categories updated

--- app/src/main/java/com/studhub/app/categories/Categories.kt
+++ app/src/main/java/com/studhub/app/categories/Categories.kt
@@ -3,14 +3,15 @@ package com.studhub.app.categories
 import com.studhub.app.domain.model.Category
 
 class Categories {
-    val Electronics: Category = Category(id = 1, name = "Electronics")
-    val SchoolsItem: Category = Category(id = 2, name = "School items")
-    val insruments: Category = Category(id = 3, name = "insruments")
-    val mobility: Category = Category(id = 4, name = "mobility")
-    val clothes: Category = Category(id = 5, name = "clothes")
-    val artdecorations: Category = Category(id = 6, name = "artdecorations")
-    val services: Category = Category(id = 7, name = "services")
-    val other: Category = Category(id = 8, name = "other")
+    val Electronics: Category = Category(id = 1, name = "Electronics", "phones/cameras/.. ")
+    val SchoolsItem: Category = Category(id = 2, name = "School items","books/pencils/bags/..")
+    val accessories: Category = Category(id = 8, name = "accessories","keys/necklaces/..")
+    val insruments: Category = Category(id = 3, name = "insruments","ear phones/guitar/..")
+    val mobility: Category = Category(id = 4, name = "mobility","bikes/scooter/..")
+    val clothes: Category = Category(id = 5, name = "clothes","pants/shirts/..")
+    val artdecorations: Category = Category(id = 6, name = "artdecorations","paintings/tapis/..")
+    val services: Category = Category(id = 7, name = "services","online services/apps/supports/..")
+    val other: Category = Category(id = 9, name = "other","other")
 }
 
 
