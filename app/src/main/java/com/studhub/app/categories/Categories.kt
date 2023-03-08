56e2558e642734d2a33af03d7576f5fceb9d63c2
tepoorte <samuel.tepoorten@epfl.ch>
2023-03-08 18:34:35 +0100

    Categories

--- app/src/main/java/com/studhub/app/categories/Categories.kt
+++ app/src/main/java/com/studhub/app/categories/Categories.kt
@@ -4,11 +4,17 @@ import com.studhub.app.domain.model.Category
 
 class Categories {
     val Electronics: Category = Category(id = 1, name = "Electronics")
-    val SchoolsItem: Category = Category(id = 1, name = "School items")
+    val SchoolsItem: Category = Category(id = 2, name = "School items")
+    val insruments: Category = Category(id = 3, name = "insruments")
+    val mobility: Category = Category(id = 4, name = "mobility")
+    val clothes: Category = Category(id = 5, name = "clothes")
+    val artdecorations: Category = Category(id = 6, name = "artdecorations")
+    val services: Category = Category(id = 7, name = "services")
+    val other: Category = Category(id = 8, name = "other")
 }
 
 
-
+/*
 enum class CategoriesEnum(val description: String) {
     ELECTRONICS("electronics"),
     SCHOOLSITEMS("school items"),
@@ -19,6 +25,6 @@ enum class CategoriesEnum(val description: String) {
     SERVICES("services"),
     OTHER("other")
 }
-
+*/
 
 
