0cb4494fc6a14a60896f9de182af7c815591b88e
tepoorte <samuel.tepoorten@epfl.ch>
2023-03-08 20:01:02 +0100

    CatRep/GetCat/GetCats

---
+++ app/src/main/java/com/studhub/app/domain/usecase/listing/GetCategories.kt
@@ -0,0 +1,13 @@
+package com.studhub.app.domain.usecase.listing
+
+
+import com.studhub.app.core.utils.ApiResponse
+import com.studhub.app.domain.model.Category
+import com.studhub.app.domain.repository.CategoryRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetCategories(private val repository: CategoryRepository) {
+    suspend operator fun invoke(): Flow<ApiResponse<List<Category>>> {
+        return repository.getCategories()
+    }
+}
