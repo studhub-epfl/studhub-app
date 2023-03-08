3ba1c719bcae545d4b5e339edae8a6b663e144cc
rnasr <raymond.nasr@epfl.ch>
2023-03-08 21:45:34 +0100

    Added basic homepage with two buttons going to empty views

--- app/src/main/java/com/studhub/app/MainActivity2.kt
+++ app/src/main/java/com/studhub/app/MainActivity2.kt
@@ -3,13 +3,19 @@ package com.studhub.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.studhub.app.ui.theme.StudHubTheme
 
 class MainActivity2 : ComponentActivity() {
@@ -19,25 +25,95 @@ class MainActivity2 : ComponentActivity() {
             StudHubTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
+                    // Going to change, customize colors later.
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
+
                 ) {
-                    Greeting("Android")
+                    AppNavigation()
                 }
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "Home") {
+        composable("Home") {
+            HomePage(
+                onAddListingClick = { navController.navigate("AddListing") },
+                onBrowseClick = { navController.navigate("Browse") }
+            )
+        }
+        composable("AddListing") {
+            AddListingScreen()
+        }
+        composable("Browse") {
+            BrowseScreen()
+        }
+    }
+}
+
+@Composable
+fun HomePage(onAddListingClick: () -> Unit, onBrowseClick: () -> Unit) {
+    StudHubTheme() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Basic Home Page",
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.h4,
+                modifier = Modifier.padding(bottom = 16.dp)
+
+            )
+            Button(
+                onClick = onAddListingClick,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Add Listing")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = onBrowseClick,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Browse")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AddListingScreen() {
+
+    StudHubTheme {
+        Text(text = "Add Listing")
+        Surface(
+            // Going to change, customize colors later.
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.primary
+
+        ) {}
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun BrowseScreen() {
     StudHubTheme {
-        Greeting("Android")
+        Text(text = "Browse")
+        Surface(
+            // Going to change, customize colors later.
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.secondaryVariant
+
+        ) {}
     }
 }
--- app/src/main/java/com/studhub/app/ui/theme/Theme.kt
+++ app/src/main/java/com/studhub/app/ui/theme/Theme.kt
@@ -5,26 +5,33 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
+    // Going to change, customize colors later.
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    secondary = Teal200,
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+)
+
+private val LightColorPalette = lightColors(
+    // Going to change, customize colors later.
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+
 )
 
 @Composable
