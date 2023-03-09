a6a22dc9acc38ba096c3adff63d882f52030ebd1
rnasr <raymond.nasr@epfl.ch>
2023-03-09 17:52:11 +0100

    First home page customization, added Dark Palette colors

--- app/src/main/java/com/studhub/app/ui/theme/Color.kt
+++ app/src/main/java/com/studhub/app/ui/theme/Color.kt
@@ -2,7 +2,15 @@ package com.studhub.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val StudHubPurple = Color(0xFF6A1B9A)
+val StudHubBlue = Color(0xFF2196F3)
+val StudHubWhite = Color(0xFFFFFFFF)
+val StudHubBlack = Color(0x000000FF)
+// Dark Palette
+val StudHubDarkGray = Color(0x1A1A1AFF )
+val StudHubDarkerGray = Color(0x121212FF)
+val StudHubLightPink = Color (0xF48FB1FF0)
+val StudHubBrightPink = Color (0xF06292FF)
+val StudHubMediumGray = Color(0x424242FF)
+// White Palette
