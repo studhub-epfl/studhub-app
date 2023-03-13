4f337fc8a0d748cb0f8e80a12f6cb8c0feec18f4
rnasr <raymond.nasr@epfl.ch>
2023-03-13 18:13:05 +0100

    Complete dark and light schemes, seperated nav controller and screens

--- app/src/main/java/com/studhub/app/ui/theme/Color.kt
+++ app/src/main/java/com/studhub/app/ui/theme/Color.kt
@@ -5,16 +5,15 @@ import androidx.compose.ui.graphics.Color
 val Teal200 = Color(0xFF03DAC5)
 val StudHubPurple = Color(0xFF6A1B9A)
 val StudHubBlue = Color(0xFF2196F3)
-val StudHubWhite = Color(0xFFFFFFFF)
-val StudHubBlack = Color(0x000000FF)
+val StudHubWhite = Color(0xFFFFFFFF) // primary text
+
+//val StudHubBlack = Color(0x000000FF)
 // Dark Palette
-val StudHubDarkGray = Color(0x1A1A1AFF )
-val StudHubDarkerGray = Color(0x121212FF)
-val StudHubLightPink = Color (0xF48FB1FF0)
-val StudHubBrightPink = Color (0xF06292FF)
-val StudHubMediumGray = Color(0x424242FF)
-// White Palette
-val StudHubLightGray = Color(0xF5F5F5FF)
-val SutdHubBrightOrange = Color(0xFF5722FF )
-val StudHubSoftOrange = Color(0xFFA07AFF)
-val StudHubGray =  Color(0x212121FF)
+val StudHubDarkBlue = Color(0xFF15202B) // background
+val StudHubLighterDarkBlue = Color(0xFF192734) // card
+val StudHubCustomGray =  Color(0xFF22303C) // hover color
+val StudHubLightGray =Color(0xFF8899A6) // secondary text
+val StudHubBurgundy = Color (0xFFB58392)
+
+
+// White Palette incomplete
--- app/src/main/java/com/studhub/app/ui/theme/Theme.kt
+++ app/src/main/java/com/studhub/app/ui/theme/Theme.kt
@@ -2,49 +2,61 @@ package com.studhub.app.ui.theme
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = StudHubDarkGray,
-    primaryVariant = StudHubDarkerGray,
-    secondary = StudHubLightPink ,
-    secondaryVariant = StudHubBrightPink,
-    background = StudHubDarkerGray,
-    surface = StudHubMediumGray,
-    onPrimary = StudHubWhite,
-    onSecondary = StudHubBlack,
-    onBackground = StudHubWhite,
-    onSurface = StudHubWhite,
+private val DarkColorPalette = darkColorScheme(
+
+    background = Color(0XFF1C1B1F),
+    onBackground = Color(0XFFE6E1E5),
+    surface = Color(0XFF1C1B1F) ,
+    onSurface = Color(0XFFE6E1E5),
+    primary = Color(0XFFD0BCFF),
+    onPrimary = Color(0xFF381E72),
+    primaryContainer = Color(0xFF4F378B),
+    onPrimaryContainer = Color(0xffEADDFF),
+    secondary = Color(0XFFCCC2DC) ,
+    onSecondary = Color(0xFF332D41) ,
+    secondaryContainer = Color (0XFF4A4458),
+    onSecondaryContainer= Color(0xFFE8DEF8),
+    tertiary = Color(0xFFEFB8C8),
+    onTertiary = Color(0Xff492532),
+    tertiaryContainer = Color(0XFF633B48),
+    onTertiaryContainer = Color(0XFFFFD8E4),
 )
 
-@SuppressLint("ConflictingOnColor")
-private val LightColorPalette = lightColors(
-    primary = StudHubWhite,
-    primaryVariant = StudHubLightGray,
-    secondary = SutdHubBrightOrange ,
-    secondaryVariant = StudHubSoftOrange,
-    background = StudHubGray,
-    surface = StudHubWhite,
-    onPrimary = StudHubDarkGray,
-    onSecondary = StudHubWhite,
-    onBackground = StudHubDarkGray,
-    onSurface = StudHubDarkGray,
+
+private val LightColorPalette = lightColorScheme(
+    background = Color(0xffFFFBFE),
+    onBackground = Color(0xff1C1B1F),
+    surface = Color(0xffFFFBFE) ,
+    onSurface = Color(0xff1C1B1F),
+    primary = Color(0xff6750A4),
+    onPrimary = Color(0xffFFFFFF),
+    primaryContainer = Color(0xffEADDFF),
+    onPrimaryContainer = Color(0xff21005D),
+    secondary = Color(0xff625B71) ,
+    onSecondary = Color(0xffFFFFFF) ,
+    secondaryContainer = Color (0xff7D5260),
+    onSecondaryContainer= Color(0xff7D5260),
+    tertiary = Color(0xff7D5260),
+    onTertiary = Color(0xffFFFFFF),
+    tertiaryContainer = Color(0xffFFD8E4),
+    onTertiaryContainer = Color(0xff31111D)
 )
 
 @Composable
 fun StudHubTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
+    val colorsScheme = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
-
     MaterialTheme(
-        colors = colors,
+        colorScheme = colorsScheme,
         typography = Typography,
         shapes = Shapes,
         content = content
