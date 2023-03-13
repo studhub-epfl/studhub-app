a74fad24c28dece1f4c7022c1ba888111cb0eb09
NervzZ <julien.jordan@epfl.ch>
2023-03-13 03:34:04 +0100

    Added new CreateListingActivity + fixed naming conflict with existing views

---
+++ app/src/main/java/com/studhub/app/CreateListingActivity.kt
@@ -0,0 +1,65 @@
+package com.studhub.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.studhub.app.presentation.ui.BasicTextField
+import com.studhub.app.presentation.ui.BigLabel
+import com.studhub.app.presentation.ui.common.input.TextBox
+import com.studhub.app.ui.theme.StudHubTheme
+
+class CreateListingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CreateListingView()
+        }
+    }
+}
+
+
+@Composable
+fun CreateListingView() {
+    StudHubTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    BigLabel(label = "Create Listing")
+                    ListingForm()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ListingForm(){
+    BasicTextField(label = "Item title")
+    TextBox(label = "Item description")
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun CreateListingPreview() {
+    CreateListingView()
+}
