a3b71d81043cff3dd8021efc388de0726b6cef6c
rnasr <raymond.nasr@epfl.ch>
2023-03-14 16:36:16 +0100

    Rebased with masters, refactored color names.

--- app/src/main/java/com/studhub/app/ui/theme/Color.kt
+++ app/src/main/java/com/studhub/app/ui/theme/Color.kt
@@ -4,32 +4,32 @@ import androidx.compose.ui.graphics.Color
 
 // general
 
-val stud_hub_pink_variant_2 = Color(0XFFFFD8E4)
-val stud_hub_black = Color(0XFF1C1B1F)
-val stud_hub_white_variant = Color(0xFFEADDFF)
+val studhub_pink_variant_2 = Color(0XFFFFD8E4)
+val studhub_black = Color(0XFF1C1B1F)
+val studhub_white_variant = Color(0xFFEADDFF)
 
 // Dark Palette
 
 
-val stud_hub_white = Color(0XFFE6E1E5)
-val stud_hub_light_pink = Color(0XFFD0BCFF)
-val stud_hub_dark_blue = Color(0xFF381E72)
-val stud_hub_violet = Color(0xFF4F378B)
-val stud_hub_pink_variant = Color(0XFFCCC2DC)
-val stud_hub_dark_violet = Color(0xFF332D41)
-val stud_hub_dark_gray = Color (0XFF4A4458)
-val stud_hub_white_variant_2= Color(0xFFE8DEF8)
-val stud_hub_pink = Color(0xFFEFB8C8)
-val stud_hub_burgundy = Color(0Xff492532)
-val stud_hub_pink_brown = Color(0XFF633B48)
+val studhub_white = Color(0XFFE6E1E5)
+val studhub_light_pink = Color(0XFFD0BCFF)
+val studhub_dark_blue = Color(0xFF381E72)
+val studhub_violet = Color(0xFF4F378B)
+val studhub_pink_variant = Color(0XFFCCC2DC)
+val studhub_dark_violet = Color(0xFF332D41)
+val studhub_dark_gray = Color (0XFF4A4458)
+val studhub_white_variant_2= Color(0xFFE8DEF8)
+val studhub_pink = Color(0xFFEFB8C8)
+val studhub_burgundy = Color(0Xff492532)
+val studhub_pink_brown = Color(0XFF633B48)
 
 
 // White Palette
 
-val stud_hub_white_v = Color(0xffFFFBFE)
-val stud_hub_violet_v = Color(0xff6750A4)
-val stud_hub_pure_white = Color(0xffFFFFFF)
-val stud_hub_flashy_blue = Color(0xff21005D)
-val stud_hub_light_gray_v = Color(0xff625B71)
-val stud_hub_burgundy_v = Color (0xff7D5260)
-val stud_hub_dark_burgundy = Color(0xff31111D)
+val studhub_white_v = Color(0xffFFFBFE)
+val studhub_violet_v = Color(0xff6750A4)
+val studhub_pure_white = Color(0xffFFFFFF)
+val studhub_flashy_blue = Color(0xff21005D)
+val studhub_light_gray_v = Color(0xff625B71)
+val studhub_burgundy_v = Color (0xff7D5260)
+val studhub_dark_burgundy = Color(0xff31111D)
--- app/src/main/java/com/studhub/app/ui/theme/Theme.kt
+++ app/src/main/java/com/studhub/app/ui/theme/Theme.kt
@@ -1,62 +1,57 @@
 package com.studhub.app.ui.theme
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColorScheme(
 
-    background = stud_hub_black ,
-    onBackground = stud_hub_white ,
-    surface = stud_hub_black,
-    onSurface = stud_hub_white,
-    primary = stud_hub_light_pink,
-    onPrimary = stud_hub_dark_blue,
-    primaryContainer = stud_hub_violet,
-    onPrimaryContainer = stud_hub_white_variant ,
-    secondary = stud_hub_pink_variant,
-    onSecondary = stud_hub_dark_violet ,
-    secondaryContainer = stud_hub_dark_gray,
-    onSecondaryContainer= stud_hub_white_variant_2,
-    tertiary = stud_hub_pink,
-    onTertiary = stud_hub_burgundy,
-    tertiaryContainer = stud_hub_pink_brown,
-    onTertiaryContainer = stud_hub_pink_variant_2
+    background = studhub_black ,
+    onBackground = studhub_white ,
+    surface = studhub_black,
+    onSurface = studhub_white,
+    primary = studhub_light_pink,
+    onPrimary = studhub_dark_blue,
+    primaryContainer = studhub_violet,
+    onPrimaryContainer = studhub_white_variant ,
+    secondary = studhub_pink_variant,
+    onSecondary = studhub_dark_violet ,
+    secondaryContainer = studhub_dark_gray,
+    onSecondaryContainer= studhub_white_variant_2,
+    tertiary = studhub_pink,
+    onTertiary = studhub_burgundy,
+    tertiaryContainer = studhub_pink_brown,
+    onTertiaryContainer = studhub_pink_variant_2
 )
 
 
 private val LightColorPalette = lightColorScheme(
-    background = stud_hub_white_v ,
-    onBackground = stud_hub_black,
-    surface = stud_hub_white_v ,
-    onSurface = stud_hub_black,
-    primary = stud_hub_violet_v,
-    onPrimary = stud_hub_pure_white,
-    primaryContainer = stud_hub_white_variant,
-    onPrimaryContainer = stud_hub_flashy_blue,
-    secondary = stud_hub_light_gray_v ,
-    onSecondary = stud_hub_pure_white ,
-    secondaryContainer =  stud_hub_burgundy_v,
-    onSecondaryContainer= stud_hub_burgundy_v,
-    tertiary = stud_hub_burgundy_v,
-    onTertiary = stud_hub_burgundy_v,
-    tertiaryContainer = stud_hub_pink_variant_2,
-    onTertiaryContainer = stud_hub_dark_burgundy
+    background = studhub_white_v ,
+    onBackground = studhub_black,
+    surface = studhub_white_v ,
+    onSurface = studhub_black,
+    primary = studhub_violet_v,
+    onPrimary = studhub_pure_white,
+    primaryContainer = studhub_white_variant,
+    onPrimaryContainer = studhub_flashy_blue,
+    secondary = studhub_light_gray_v ,
+    onSecondary = studhub_pure_white ,
+    secondaryContainer =  studhub_burgundy_v,
+    onSecondaryContainer= studhub_burgundy_v,
+    tertiary = studhub_burgundy_v,
+    onTertiary = studhub_burgundy_v,
+    tertiaryContainer = studhub_pink_variant_2,
+    onTertiaryContainer = studhub_dark_burgundy
 )
 
 
 
 @Composable
 fun StudHubTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colorsScheme = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colorsScheme = if (darkTheme) DarkColorPalette else LightColorPalette
+
     MaterialTheme(
         colorScheme = colorsScheme,
         typography = Typography,
