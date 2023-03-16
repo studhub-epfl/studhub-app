c408508d5b993e536931b480804833e4d3733abe
NervzZ <julien.jordan@epfl.ch>
2023-03-16 23:49:13 +0100

    browse list basic structure done, waiting on other PR to go through to get needed components

--- app/src/main/java/com/studhub/app/BrowseActivity.kt
+++ app/src/main/java/com/studhub/app/BrowseActivity.kt
@@ -4,32 +4,71 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.studhub.app.core.utils.ApiResponse
+import com.studhub.app.data.repository.ListingRepositoryImpl
+import com.studhub.app.domain.model.Category
+import com.studhub.app.domain.model.Listing
+import com.studhub.app.domain.model.User
+import com.studhub.app.domain.usecase.listing.GetListings
 import com.studhub.app.ui.theme.StudHubTheme
 
 class BrowseActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BrowseList()
+            var listings = emptyList<Listing>()
+            LaunchedEffect(listings) {
+                listings = getAllListings()
+            }
+            BrowseList(listings)
         }
     }
 }
 
+private suspend fun getAllListings(): List<Listing> {
+    val listingRepository = ListingRepositoryImpl()
+    val getListings = GetListings(listingRepository)
+    lateinit var listings: List<Listing>
+    getListings().collect {
+        when (it) {
+            is ApiResponse.Success -> {
+                listings = it.data
+            }
+            is ApiResponse.Failure -> {/* TODO - show error status on screen */
+            }
+            is ApiResponse.Loading -> {}
+        }
+    }
+    return listings
+}
+
 @Composable
-fun BrowseList() {
+fun BrowseList(listings: List<Listing>) {
     StudHubTheme {
-        // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Text("test")
+            LazyColumn(
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp)
+            ) {
+                items(listings) { listing ->
+                    Text(listing.name)
+                    Divider()
+                }
+            }
         }
     }
 }
@@ -37,7 +76,27 @@ fun BrowseList() {
 @Preview(showBackground = true)
 @Composable
 fun BrowseActivityPreview() {
+    val listings = listOf(
+        Listing(
+            name = "Algebra for the dummies",
+            seller = User(firstName = "Jacky", lastName = "Chan"),
+            categories = listOf(Category(name = "Books")),
+            price = 34.50F
+        ),
+        Listing(
+            name = "Brand new Nike Air One",
+            seller = User(firstName = "Kristina", lastName = "Gordova"),
+            categories = listOf(Category(name = "Clothing")),
+            price = 194.25F
+        ),
+        Listing(
+            name = "Super VTT 2000 with custom paint",
+            seller = User(firstName = "Marc", lastName = "Marquez"),
+            categories = listOf(Category(name = "Mobility")),
+            price = 1500F
+        )
+    )
     StudHubTheme {
-        BrowseList()
+        BrowseList(listings = listings)
     }
 }
