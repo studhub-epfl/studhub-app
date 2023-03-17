4f829b641a83749f2496fcbb7f8faf6a2dcb8ef6
Edouard Michelin <edouard.michelin15@gmail.com>
2023-03-17 07:46:53 +0100

    Add dependency injection provider

---
+++ app/src/main/java/com/studhub/app/di/AppModule.kt
@@ -0,0 +1,114 @@
+package com.studhub.app.di
+
+import android.app.Application
+import android.content.Context
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.studhub.app.R
+import com.studhub.app.core.Constants.SIGN_IN_REQUEST
+import com.studhub.app.core.Constants.SIGN_UP_REQUEST
+import com.studhub.app.data.repository.AuthRepositoryImpl
+import com.studhub.app.data.repository.UserRepositoryImpl
+import com.studhub.app.domain.repository.AuthRepository
+import com.studhub.app.domain.repository.UserRepository
+import com.studhub.app.domain.usecase.user.GetCurrentUser
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Named
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class AppModule {
+    @Provides
+    fun provideFirebaseAuth() = Firebase.auth
+
+    @Provides
+    fun provideFirebaseDatabase() = Firebase.database
+
+    @Provides
+    fun provideOneTapClient(
+        @ApplicationContext
+        context: Context
+    ) = Identity.getSignInClient(context)
+
+    @Provides
+    @Named(SIGN_IN_REQUEST)
+    fun provideSignInRequest(
+        app: Application
+    ) = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(app.getString(R.string.default_web_client_id))
+                .setFilterByAuthorizedAccounts(true)
+                .build()
+        )
+        .setAutoSelectEnabled(true)
+        .build()
+
+    @Provides
+    @Named(SIGN_UP_REQUEST)
+    fun provideSignUpRequest(
+        app: Application
+    ) = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(app.getString(R.string.default_web_client_id))
+                .setFilterByAuthorizedAccounts(false)
+                .build()
+        )
+        .build()
+
+    @Provides
+    fun provideGoogleSignInOptions(
+        app: Application
+    ) = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken(app.getString(R.string.default_web_client_id))
+        .requestEmail()
+        .build()
+
+    @Provides
+    fun provideGoogleSignInClient(
+        app: Application,
+        options: GoogleSignInOptions
+    ) = GoogleSignIn.getClient(app, options)
+
+    @Provides
+    fun provideAuthRepository(
+        auth: FirebaseAuth,
+        oneTapClient: SignInClient,
+        @Named(SIGN_IN_REQUEST)
+        signInRequest: BeginSignInRequest,
+        @Named(SIGN_UP_REQUEST)
+        signUpRequest: BeginSignInRequest,
+        db: FirebaseDatabase
+    ): AuthRepository = AuthRepositoryImpl(
+        auth = auth,
+        oneTapClient = oneTapClient,
+        signInRequest = signInRequest,
+        signUpRequest = signUpRequest,
+        db = db
+    )
+
+    @Provides
+    fun provideUserRepository(): UserRepository = UserRepositoryImpl()
+
+    @Provides
+    fun provideGetCurrentUserUseCase(
+        userRepository: UserRepository,
+        authRepository: AuthRepository
+    ): GetCurrentUser = GetCurrentUser(userRepository, authRepository)
+
+}
