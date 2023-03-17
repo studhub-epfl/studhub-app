37c3f2c0a2f9f2dcf9f63107c58d2f276e55bd45
NervzZ <julien.jordan@epfl.ch>
2023-03-17 09:13:53 +0100

    Will do for the demo for now

--- app/src/main/java/com/studhub/app/BrowseActivity.kt
+++ app/src/main/java/com/studhub/app/BrowseActivity.kt
@@ -33,10 +33,35 @@ class BrowseActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            /* TODO - unable to make that work for sprint1, implement it later
             LaunchedEffect(listings) {
                 listings = getAllListings()
             }
+            */
+            val listings = listOf(
+                Listing(
+                    name = "Algebra for the dummies",
+                    seller = User(firstName = "Jacky", lastName = "Chan"),
+                    categories = listOf(Category(name = "Books")),
+                    description = "Really great book to learn Algebra for entry level readers.",
+                    price = 34.50F
+                ),
+                Listing(
+                    name = "Brand new Nike Air One",
+                    seller = User(firstName = "Kristina", lastName = "Gordova"),
+                    categories = listOf(Category(name = "Clothing")),
+                    description = "Branx new shoes, full white and ready for any custom work if needed.",
+                    price = 194.25F
+                ),
+                Listing(
+                    name = "Super VTT 2000 with custom paint",
+                    seller = User(firstName = "Marc", lastName = "Marquez"),
+                    categories = listOf(Category(name = "Mobility")),
+                    description = "Robust bike for downhill riding and will " +
+                            "look absolutely unique with this custom paint work",
+                    price = 1500F
+                )
+            )
             BrowseList(listings)
         }
     }
