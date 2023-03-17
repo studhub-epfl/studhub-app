caff648eb326027f61b47b03cc9d5c73114dbc87
Edouard Michelin <edouard.michelin15@gmail.com>
2023-03-17 07:46:05 +0100

    Link auth to other views

--- app/src/main/java/com/studhub/app/MainActivity.kt
+++ app/src/main/java/com/studhub/app/MainActivity.kt
@@ -2,17 +2,28 @@ package com.studhub.app
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.studhub.app.presentation.auth.AuthViewModel
 import com.studhub.app.ui.theme.StudHubTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavHostController
+    private val viewModel by viewModels<AuthViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            navController = rememberNavController()
             StudHubTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -20,11 +31,20 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppNavigation()
+                    AppNavigation(navController = navController)
                 }
+                checkAuthState()
             }
         }
     }
+
+    private fun checkAuthState() {
+        if (viewModel.isUserAuthenticated) {
+            handleLoginComplete()
+        }
+    }
+
+    private fun handleLoginComplete() = navController.navigate("Home")
 }
 
 
---
+++ app/src/main/java/com/studhub/app/StudHubApp.kt
@@ -0,0 +1,7 @@
+package com.studhub.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StudHubApp: Application()
