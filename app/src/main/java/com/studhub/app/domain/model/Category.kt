8bea37104553b0c9154c225b8bb17a2e7a67c288
unknown <edouard.michelin15@gmail.com>
2023-03-21 22:46:18 +0100

    Remove unnecessary 'saver' field

--- app/src/main/java/com/studhub/app/domain/model/Category.kt
+++ app/src/main/java/com/studhub/app/domain/model/Category.kt
@@ -6,11 +6,5 @@ import androidx.compose.runtime.saveable.listSaver
 data class Category(
     val id: String = "",
     val name: String = "",
-    val description: String = "",
-
-    val CategorySaver: Saver<Category, Any> = listSaver(
-        save = { listOf(it.id, it.name) },
-        restore = { Category(id = it[0] as String, name = it[1] as String) }
-    )
-
+    val description: String = ""
 )
