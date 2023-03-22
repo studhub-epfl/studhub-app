d0984b09807ed34e0c0a4d50c8e8863882c58a1a
unknown <edouard.michelin15@gmail.com>
2023-03-22 05:38:06 +0100

    Add wraper activities

--- app/src/main/java/com/studhub/app/wrapper/AboutActivity.kt
+++ app/src/main/java/com/studhub/app/wrapper/AboutActivity.kt
@@ -3,17 +3,14 @@ package com.studhub.app.wrapper
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.studhub.app.AppNavigation
-import com.studhub.app.presentation.home.HomeScreen
-import dagger.hilt.android.AndroidEntryPoint
+import com.studhub.app.presentation.about.AboutScreen
 
-@AndroidEntryPoint
-class NavigationActivity: AppCompatActivity() {
+class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            AppNavigation(startDestination = "Home")
+            AboutScreen()
         }
     }
 }
--- app/src/main/java/com/studhub/app/wrapper/CartActivity.kt
+++ app/src/main/java/com/studhub/app/wrapper/CartActivity.kt
@@ -3,17 +3,14 @@ package com.studhub.app.wrapper
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.studhub.app.AppNavigation
-import com.studhub.app.presentation.home.HomeScreen
-import dagger.hilt.android.AndroidEntryPoint
+import com.studhub.app.presentation.cart.CartScreen
 
-@AndroidEntryPoint
-class NavigationActivity: AppCompatActivity() {
+class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            AppNavigation(startDestination = "Home")
+            CartScreen()
         }
     }
 }
