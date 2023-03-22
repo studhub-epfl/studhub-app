cbab11759f89444d6e872871a5b8df9c521b96ff
unknown <edouard.michelin15@gmail.com>
2023-03-22 04:19:29 +0100

    Add wrapper activities for screens

---
+++ app/src/main/java/com/studhub/app/wrapper/NavigationActivity.kt
@@ -0,0 +1,19 @@
+package com.studhub.app.wrapper
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import com.studhub.app.AppNavigation
+import com.studhub.app.presentation.home.HomeScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NavigationActivity(): AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AppNavigation(startDestination = "Home")
+        }
+    }
+}
