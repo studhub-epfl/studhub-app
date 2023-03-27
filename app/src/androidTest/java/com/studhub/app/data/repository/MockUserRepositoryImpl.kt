4a751b42a4d200e96ef74c2eedb33f3dfc86df8d
Edouard <edouard.waked@epfl.ch>
2023-03-27 02:41:28 +0200

    Added new tests for new favorite feature methods

--- app/src/androidTest/java/com/studhub/app/data/repository/MockUserRepositoryImpl.kt
+++ app/src/androidTest/java/com/studhub/app/data/repository/MockUserRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.studhub.app.data.repository
 import com.studhub.app.core.utils.ApiResponse
 import com.studhub.app.domain.model.Listing
 import com.studhub.app.domain.model.User
-import com.studhub.app.domain.repository.ListingRepository
 import com.studhub.app.domain.repository.UserRepository
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -11,8 +10,9 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Singleton
 
 @Singleton
-class MockUserRepositoryImpl: UserRepository {
+class MockUserRepositoryImpl : UserRepository {
     private val userDB = HashMap<String, User>()
+    private val listingDB = HashMap<String, Listing>()
 
     init {
         userDB[MockAuthRepositoryImpl.loggedInUser.id] = MockAuthRepositoryImpl.loggedInUser
@@ -21,6 +21,7 @@ class MockUserRepositoryImpl: UserRepository {
     override suspend fun createUser(user: User): Flow<ApiResponse<User>> {
         return flow {
             emit(ApiResponse.Loading)
+            delay(1000)
             userDB[user.id] = user
             emit(ApiResponse.Success(user))
         }
@@ -29,6 +30,7 @@ class MockUserRepositoryImpl: UserRepository {
     override suspend fun getUser(userId: String): Flow<ApiResponse<User>> {
         return flow {
             emit(ApiResponse.Loading)
+            delay(1000)
             if (userDB.containsKey(userId))
                 emit(ApiResponse.Success(userDB.getValue(userId)))
             else
@@ -36,9 +38,13 @@ class MockUserRepositoryImpl: UserRepository {
         }
     }
 
-    override suspend fun updateUserInfo(userId: String, updatedUser: User): Flow<ApiResponse<User>> {
+    override suspend fun updateUserInfo(
+        userId: String,
+        updatedUser: User
+    ): Flow<ApiResponse<User>> {
         return flow {
             emit(ApiResponse.Loading)
+            delay(1000)
             if (userDB.containsKey(userId)) {
                 val newListing = updatedUser.copy(id = userId)
                 userDB[userId] = newListing
@@ -50,7 +56,80 @@ class MockUserRepositoryImpl: UserRepository {
 
     override suspend fun removeUser(userId: String): Flow<ApiResponse<Boolean>> {
         return flow {
-            emit(ApiResponse.Success(true))
+            emit(ApiResponse.Loading)
+            delay(1000)
+            if (userDB.containsKey(userId)) {
+                userDB.remove(userId)
+                emit(ApiResponse.Success(true))
+            } else {
+                emit(ApiResponse.Failure("No entry for this key"))
+            }
+        }
+    }
+
+    override suspend fun addFavoriteListing(
+        userId: String,
+        favListingId: String
+    ): Flow<ApiResponse<User>> {
+        return flow {
+            emit(ApiResponse.Loading)
+            delay(1000)
+            if (userDB.containsKey(userId)) {
+                val user = userDB.getValue(userId)
+                if (user.favoriteListings.contains(favListingId)) {
+                    emit(ApiResponse.Failure("Listing is already a favorite"))
+                } else {
+                    val updatedFavoriteListings =
+                        user.favoriteListings.toMutableList().apply { add(favListingId) }
+                    val updatedUser = user.copy(favoriteListings = updatedFavoriteListings)
+                    userDB[userId] = updatedUser
+                    emit(ApiResponse.Success(updatedUser))
+                }
+            } else {
+                emit(ApiResponse.Failure("No entry for this key"))
+            }
+        }
+    }
+
+    override suspend fun removeFavoriteListing(
+        userId: String,
+        favListingId: String
+    ): Flow<ApiResponse<Boolean>> {
+        return flow {
+            emit(ApiResponse.Loading)
+            delay(1000)
+            if (userDB.containsKey(userId)) {
+                val user = userDB[userId]!!
+                if (!user.favoriteListings.contains(favListingId)) {
+                    emit(ApiResponse.Failure("Listing is not a favorite"))
+                    return@flow
+                }
+
+                val updatedFavoriteListings =
+                    user.favoriteListings.toMutableList().apply { remove(favListingId) }
+                val updatedUser = user.copy(favoriteListings = updatedFavoriteListings)
+                userDB[userId] = updatedUser
+
+                emit(ApiResponse.Success(true))
+            } else {
+                emit(ApiResponse.Failure("No entry for this key"))
+            }
+        }
+    }
+
+    override suspend fun getFavoriteListings(userId: String): Flow<ApiResponse<List<Listing>>> {
+        return flow {
+            emit(ApiResponse.Loading)
+            delay(1000)
+            if (userDB.containsKey(userId)) {
+                val user = userDB[userId]!!
+                val favoriteListings = user.favoriteListings.mapNotNull { listingId ->
+                    listingDB[listingId]
+                }
+                emit(ApiResponse.Success(favoriteListings))
+            } else {
+                emit(ApiResponse.Failure("No entry for this key"))
+            }
         }
     }
 }
--- app/src/androidTest/java/com/studhub/app/domain/usecase/UserUseCaseTest.kt
+++ app/src/androidTest/java/com/studhub/app/domain/usecase/UserUseCaseTest.kt
@@ -3,11 +3,11 @@ package com.studhub.app.domain.usecase
 import com.google.android.gms.auth.api.identity.BeginSignInResult
 import com.google.firebase.auth.AuthCredential
 import com.studhub.app.core.utils.ApiResponse
+import com.studhub.app.domain.model.Listing
 import com.studhub.app.domain.model.User
 import com.studhub.app.domain.repository.AuthRepository
 import com.studhub.app.domain.repository.UserRepository
-import com.studhub.app.domain.usecase.user.GetUser
-import com.studhub.app.domain.usecase.user.UpdateCurrentUserInfo
+import com.studhub.app.domain.usecase.user.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -17,17 +17,16 @@ import org.junit.After
 import org.junit.Assert
 import org.junit.Test
 import kotlin.random.Random
-import kotlin.random.nextLong
 
 class UserUseCaseTest {
     private val userDB = HashMap<String, User>()
-    private val loggedInUser = User(id = "user-uid", userName = "John Doe" )
+    private val loggedInUser = User(id = "user-uid", userName = "John Doe")
 
     init {
         userDB[loggedInUser.id] = loggedInUser
     }
 
-    private val authRepo: AuthRepository = object : AuthRepository {
+    private val authRepository: AuthRepository = object : AuthRepository {
         override val isUserAuthenticatedInFirebase: Boolean
             get() = true
         override val currentUserUid: String
@@ -42,6 +41,8 @@ class UserUseCaseTest {
         override suspend fun signOut(): Flow<ApiResponse<Boolean>> = flowOf(ApiResponse.Loading)
 
     }
+
+    private val listingDB = HashMap<String, Listing>()
 
     private val repository: UserRepository = object : UserRepository {
         override suspend fun createUser(user: User): Flow<ApiResponse<User>> {
@@ -82,7 +83,80 @@ class UserUseCaseTest {
 
         override suspend fun removeUser(userId: String): Flow<ApiResponse<Boolean>> {
             return flow {
-                emit(ApiResponse.Success(true))
+                emit(ApiResponse.Loading)
+                delay(1000)
+                if (userDB.containsKey(userId)) {
+                    userDB.remove(userId)
+                    emit(ApiResponse.Success(true))
+                } else {
+                    emit(ApiResponse.Failure("No entry for this key"))
+                }
+            }
+        }
+
+        override suspend fun addFavoriteListing(
+            userId: String,
+            favListingId: String
+        ): Flow<ApiResponse<User>> {
+            return flow {
+                emit(ApiResponse.Loading)
+                delay(1000)
+                if (userDB.containsKey(userId)) {
+                    val user = userDB.getValue(userId)
+                    if (user.favoriteListings.contains(favListingId)) {
+                        emit(ApiResponse.Failure("Listing is already a favorite"))
+                    } else {
+                        val updatedFavoriteListings =
+                            user.favoriteListings.toMutableList().apply { add(favListingId) }
+                        val updatedUser = user.copy(favoriteListings = updatedFavoriteListings)
+                        userDB[userId] = updatedUser
+                        emit(ApiResponse.Success(updatedUser))
+                    }
+                } else {
+                    emit(ApiResponse.Failure("No entry for this key"))
+                }
+            }
+        }
+
+        override suspend fun removeFavoriteListing(
+            userId: String,
+            favListingId: String
+        ): Flow<ApiResponse<Boolean>> {
+            return flow {
+                emit(ApiResponse.Loading)
+                delay(1000)
+                if (userDB.containsKey(userId)) {
+                    val user = userDB[userId]!!
+                    if (!user.favoriteListings.contains(favListingId)) {
+                        emit(ApiResponse.Failure("Listing is not a favorite"))
+                        return@flow
+                    }
+
+                    val updatedFavoriteListings =
+                        user.favoriteListings.toMutableList().apply { remove(favListingId) }
+                    val updatedUser = user.copy(favoriteListings = updatedFavoriteListings)
+                    userDB[userId] = updatedUser
+
+                    emit(ApiResponse.Success(true))
+                } else {
+                    emit(ApiResponse.Failure("No entry for this key"))
+                }
+            }
+        }
+
+        override suspend fun getFavoriteListings(userId: String): Flow<ApiResponse<List<Listing>>> {
+            return flow {
+                emit(ApiResponse.Loading)
+                delay(1000)
+                if (userDB.containsKey(userId)) {
+                    val user = userDB[userId]!!
+                    val favoriteListings = user.favoriteListings.mapNotNull { listingId ->
+                        listingDB[listingId]
+                    }
+                    emit(ApiResponse.Success(favoriteListings))
+                } else {
+                    emit(ApiResponse.Failure("No entry for this key"))
+                }
             }
         }
 
@@ -116,7 +190,7 @@ class UserUseCaseTest {
 
     @Test
     fun updateUserUseCaseUpdatesCorrectEntryInGivenRepository() = runBlocking {
-        val updateCurrentUserInfo = UpdateCurrentUserInfo(repository, authRepo)
+        val updateCurrentUserInfo = UpdateCurrentUserInfo(repository, authRepository)
 
         val userId = Random.nextLong().toString()
         val userName = Random.nextLong().toString()
@@ -146,6 +220,84 @@ class UserUseCaseTest {
                         result.userName,
                         userName
                     )
+                }
+                is ApiResponse.Failure -> Assert.fail("Request failure")
+                is ApiResponse.Loading -> {}
+            }
+        }
+    }
+
+    @Test
+    fun addFavoriteListingAddsCorrectListingToUserFavorites() = runBlocking {
+        val addFavoriteListing = AddFavoriteListing(repository, authRepository)
+        val userId = authRepository.currentUserUid
+        val listingId = Random.nextLong().toString()
+        userDB[userId] = User(id = userId, userName = "Test User", favoriteListings = emptyList())
+
+        addFavoriteListing(listingId).collect { response ->
+            when (response) {
+                is ApiResponse.Success -> {
+                    val user = response.data
+                    Assert.assertNotNull(user)
+                    Assert.assertEquals(listOf(listingId), user.favoriteListings)
+                }
+                is ApiResponse.Failure -> Assert.fail("Request failure")
+                is ApiResponse.Loading -> {}
+            }
+        }
+    }
+
+    @Test
+    fun removeFavoriteListingRemovesCorrectListingFromUserFavorites() = runBlocking {
+        val removeFavoriteListing = RemoveFavoriteListing(repository, authRepository)
+
+        val userId = authRepository.currentUserUid
+        val listingId1 = Random.nextLong().toString()
+        val listingId2 = Random.nextLong().toString()
+        userDB[userId] = User(
+            id = userId,
+            userName = "Test User",
+            favoriteListings = listOf(listingId1, listingId2)
+        )
+
+        removeFavoriteListing(listingId1).collect { response ->
+            when (response) {
+                is ApiResponse.Success -> {
+                    val result = response.data
+                    Assert.assertNotNull(result)
+                    Assert.assertTrue(result)
+                    val user = userDB.getValue(userId)
+                    Assert.assertEquals(listOf(listingId2), user.favoriteListings)
+                }
+                is ApiResponse.Failure -> Assert.fail("Request failure")
+                is ApiResponse.Loading -> {}
+            }
+        }
+    }
+
+    @Test
+    fun getFavoriteListingsReturnsCorrectListings() = runBlocking {
+        val getFavoriteListings = GetFavoriteListings(repository, authRepository)
+
+        val userId = authRepository.currentUserUid
+        val listing1 = Listing(id = Random.nextLong().toString(), name = "Test Listing 1")
+        val listing2 = Listing(id = Random.nextLong().toString(), name = "Test Listing 2")
+        listingDB[listing1.id] = listing1
+        listingDB[listing2.id] = listing2
+        userDB[userId] = User(
+            id = userId,
+            userName = "Test User",
+            favoriteListings = listOf(listing1.id, listing2.id)
+        )
+
+        getFavoriteListings().collect { response ->
+            when (response) {
+                is ApiResponse.Success -> {
+                    val result = response.data
+                    Assert.assertNotNull(result)
+                    Assert.assertEquals(2, result.size)
+                    Assert.assertTrue(result.contains(listing1))
+                    Assert.assertTrue(result.contains(listing2))
                 }
                 is ApiResponse.Failure -> Assert.fail("Request failure")
                 is ApiResponse.Loading -> {}
