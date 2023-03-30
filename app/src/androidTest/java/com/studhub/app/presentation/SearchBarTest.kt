c70a5a1dce4c822ec642c133a3bd2e137f0bbd88
tepoorte <samuel.tepoorten@epfl.ch>
2023-03-31 00:49:27 +0200

    SearchBar implemented, Browscreen updated, Searchbar tests, task2 finished

---
+++ app/src/androidTest/java/com/studhub/app/presentation/SearchBarTest.kt
@@ -0,0 +1,38 @@
+package com.studhub.app.presentation.ui.common
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.studhub.app.presentation.ui.common.input.SearchBar
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SearchBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testSearchBar() {
+        composeTestRule.setContent {
+            SearchBar()
+        }
+
+        // Check that the menu button is displayed and clickable
+        composeTestRule.onNodeWithContentDescription("Menu button").assertIsDisplayed().performClick()
+
+        // Check that the search bar is displayed and has the correct label
+        composeTestRule.onNodeWithText("Search...").assertIsDisplayed()
+
+        // Type a query into the search bar
+        composeTestRule.onNodeWithText("Search...").performTextInput("Test query")
+        composeTestRule.onNodeWithText("Test query").assertIsDisplayed().assertIsFocused()
+
+        // Clear the query by clicking the clear button
+        composeTestRule.onNodeWithContentDescription("Clear button").assertIsDisplayed().performClick()
+        composeTestRule.onNodeWithText("").assertIsDisplayed().assertIsFocused()
+    }
+}
--- app/src/main/java/com/studhub/app/domain/repository/ListingRepository.kt
+++ app/src/main/java/com/studhub/app/domain/repository/ListingRepository.kt
@@ -1,18 +1,49 @@
 package com.studhub.app.domain.repository
 
 import com.studhub.app.core.utils.ApiResponse
+import com.studhub.app.domain.model.Conversation
 import com.studhub.app.domain.model.Listing
 import kotlinx.coroutines.flow.Flow
 
 interface ListingRepository {
+    /**
+     * create a [listing] on the database of Firebase
+     * @param [listing] the listing we want to create
+     * @return A [Flow] of [ApiResponse] with the last one containing the [Listing] pushed to the database on success
+     */
     suspend fun createListing(listing: Listing): Flow<ApiResponse<Listing>>
 
+    /**
+     * get a list of [listing] with all the listings on the database of Firebase
+     * @return A [Flow] of [ApiResponse] with the last one containing the list of [Listing] pushed to the database on success
+     */
     suspend fun getListings(): Flow<ApiResponse<List<Listing>>>
+    /**
+     * get a  [listing] with all the listings on the database of Firebase
+     * @param [listingId] the listingId we want to match
+     * @return A [Flow] of [ApiResponse] with the last one containing the [Listing] pushed to the database on success
+     */
     suspend fun getListing(listingId: String): Flow<ApiResponse<Listing>>
 
+    /**
+     * get a list of [listing] with all the listings on the database of Firebase with the constraint given on parameter
+     * @param [keyword] the constraint we want the listings to filter
+     * @return A [Flow] of [ApiResponse] with the last one containing the filtered list of [Listing] pushed to the database on success
+     */
     suspend fun getListingsBySearch(keyword: String): Flow<ApiResponse<List<Listing>>>
 
+    /**
+     * update a listing with the given [listingId]
+     * @param [listingId] the listingId we want to match
+     * @param [updatedListing] the Listing we want to replace
+     * @return A [Flow] of [ApiResponse] with the last one containing the updated [Listing] pushed to the database on success
+     */
     suspend fun updateListing(listingId: String, updatedListing: Listing): Flow<ApiResponse<Listing>>
 
+    /**
+     * remove a listing with the given [listingId]
+     * @param [listingId] the listingId we want to match
+     * @return A [Flow] of [ApiResponse] with the last one containing the [Boolean] value of the resulting operation
+     */
     suspend fun removeListing(listingId: String): Flow<ApiResponse<Boolean>>
 }
