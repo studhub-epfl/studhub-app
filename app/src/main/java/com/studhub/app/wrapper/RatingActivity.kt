ec8ecc7191b5a6fa70ada26b9839dc618ba940ff
rnasr <raymond.nasr@epfl.ch>
2023-04-20 03:32:44 +0200

    Complete view-model, screen. Added use cases to AppModule.kt. Added RatingActivity.kt wrapper

---
+++ app/src/main/java/com/studhub/app/wrapper/RatingActivity.kt
@@ -0,0 +1,19 @@
+package com.studhub.app.wrapper
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import com.studhub.app.presentation.ratings.UserRatingScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RatingActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            UserRatingScreen()
+        }
+    }
+}
