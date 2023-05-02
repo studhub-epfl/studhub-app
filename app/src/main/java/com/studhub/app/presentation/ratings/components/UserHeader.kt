e8a80356f7905de5aed31be70522a00aab97b33c
rnasr <raymond.nasr@epfl.ch>
2023-05-02 16:19:24 +0200

    Added custom image icons for thumbs up and down.

--- app/src/main/java/com/studhub/app/presentation/ratings/components/UserHeader.kt
+++ app/src/main/java/com/studhub/app/presentation/ratings/components/UserHeader.kt
@@ -1,16 +1,26 @@
 package com.studhub.app.presentation.ratings.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.studhub.app.core.utils.ApiResponse
 import com.studhub.app.domain.model.User
+import com.studhub.app.presentation.ui.common.icons.CustomThumbsDown
+import com.studhub.app.presentation.ui.common.icons.CustomThumbsUp
+import androidx.compose.foundation.Image
 
 @Composable
 fun UserHeader(
@@ -22,6 +32,7 @@ fun UserHeader(
         is ApiResponse.Loading -> {
             CircularProgressIndicator(modifier = Modifier.padding(top = 16.dp, start = 16.dp))
         }
+
         is ApiResponse.Success -> {
             Text(
                 text = userResponse.data.firstName + " " + userResponse.data.lastName,
@@ -34,13 +45,16 @@ fun UserHeader(
                 modifier = Modifier
                     .padding(horizontal = 16.dp)
             ) {
-                Text("Thumbs Up: $thumbsUpCount", fontSize = 20.sp)
+                Image(painter = CustomThumbsUp(), contentDescription = "Thumbs Up"
+                , modifier = Modifier.size(width = 36.dp, height = 36.dp))
+                Text(" $thumbsUpCount", fontSize = 20.sp)
                 Spacer(modifier = Modifier.width(16.dp))
-                Text(
-                    "Thumbs Down: $thumbsDownCount", fontSize = 20.sp
-                )
+                Image(painter = CustomThumbsDown(), contentDescription = "Thumbs Down",
+                    modifier = Modifier.size(width = 36.dp, height = 36.dp))
+                Text(" $thumbsDownCount", fontSize = 20.sp)
             }
         }
+
         else -> {
             Text("Error", modifier = Modifier.padding(top = 16.dp, start = 16.dp))
         }
