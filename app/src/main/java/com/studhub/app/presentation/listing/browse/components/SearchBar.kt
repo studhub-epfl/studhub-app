f01ed4e635d18838ef30d17fd999922f335c2de7
NervzZ <julien.jordan@epfl.ch>
2023-05-03 17:15:46 +0200

    refactored a few things for readability, removed preview and listings sample as there was no way I could think of to make the preview work with current implementation

--- app/src/main/java/com/studhub/app/presentation/listing/browse/components/SearchBar.kt
+++ app/src/main/java/com/studhub/app/presentation/listing/browse/components/SearchBar.kt
@@ -1,19 +1,15 @@
-package com.studhub.app.presentation.ui.common.input
+package com.studhub.app.presentation.listing.browse.components
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
