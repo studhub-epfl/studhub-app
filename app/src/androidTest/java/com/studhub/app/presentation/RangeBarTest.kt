ee9ec7842b661db7dc6ac85ba4733fc91fde9381
tepoorte <samuel.tepoorten@epfl.ch>
2023-05-04 11:10:12 +0200

    tests for RangeBar and SearchBar working again

---
+++ app/src/androidTest/java/com/studhub/app/presentation/RangeBarTest.kt
@@ -0,0 +1,39 @@
+package com.studhub.app.presentation.ui.common.input
+
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4::class)
+class RangeBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+
+    @Test
+    fun rangeBarDisplaysLabel() {
+
+
+        composeTestRule.setContent {
+            RangeBar("MIN....CHF",
+                search = rememberSaveable { mutableStateOf("")
+
+                },
+                onSearch = {}
+            )
+        }
+        composeTestRule.onNodeWithText("MIN....CHF").performTextInput("5")
+    }
+
+
+    // Add more tests as needed
+}
