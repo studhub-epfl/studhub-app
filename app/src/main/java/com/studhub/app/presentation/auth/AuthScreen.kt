b051f544d90b530489c899fafd4cc4c2d4de465e
edouardmichelin <42861545+edouardmichelin@users.noreply.github.com>
2023-05-04 15:09:32 +0200

    Feature/remove google one tap (#124)
    Completely remove Google One-Tap.
    Email/password authentication is sufficient, plus google one-tap does not work really well with EPFL accounts and does not work at all with UNIL accounts.
    This PR also fixes the annoying floating loading circle on the sign-in screen and displays the correct top app bar the sign-in page.
    
    * Remove Google One-Tap Sign-In
    
    * Fix wrong top app bar on auth screen
    
    * Remove Google One-Tap from tests

--- app/src/main/java/com/studhub/app/presentation/auth/AuthScreen.kt
+++ app/src/main/java/com/studhub/app/presentation/auth/AuthScreen.kt
@@ -1,16 +1,9 @@
 package com.studhub.app.presentation.auth
 
-import android.app.Activity.RESULT_OK
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.IntentSenderRequest
-import androidx.activity.result.contract.ActivityResultContracts.StartIntentSenderForResult
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.google.android.gms.auth.api.identity.BeginSignInResult
-import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.GoogleAuthProvider.getCredential
 import com.studhub.app.core.Globals
 import com.studhub.app.presentation.auth.components.*
 
@@ -18,7 +11,6 @@ import com.studhub.app.presentation.auth.components.*
 @Composable
 fun AuthScreen(
     viewModel: AuthViewModel = hiltViewModel(),
-    onLoginComplete: (isNewUser: Boolean) -> Unit,
     navigateToForgotPasswordScreen: () -> Unit,
     navigateToSignUpScreen: () -> Unit
 ) {
@@ -36,40 +28,6 @@ fun AuthScreen(
                 navigateToForgotPasswordScreen = navigateToForgotPasswordScreen,
                 navigateToSignUpScreen = navigateToSignUpScreen
             )
-            AuthContent(
-                padding = padding,
-                oneTapSignIn = {
-                    viewModel.oneTapSignIn()
-                }
-            )
         }
-    )
-
-    val launcher = rememberLauncherForActivityResult(StartIntentSenderForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {
-            try {
-                val credentials = viewModel.oneTapClient.getSignInCredentialFromIntent(result.data)
-                val googleIdToken = credentials.googleIdToken
-                val googleCredentials = getCredential(googleIdToken, null)
-                viewModel.signInWithGoogle(googleCredentials)
-            } catch (e: ApiException) {
-                print(e)
-            }
-        }
-    }
-
-    fun launch(signInResult: BeginSignInResult) {
-        val intent = IntentSenderRequest.Builder(signInResult.pendingIntent.intentSender).build()
-        launcher.launch(intent)
-    }
-
-    OneTapSignIn(
-        launch = {
-            launch(it)
-        }
-    )
-
-    SignInWithGoogle(
-        onSignIn = onLoginComplete
     )
 }
--- app/src/main/java/com/studhub/app/presentation/auth/AuthViewModel.kt
+++ app/src/main/java/com/studhub/app/presentation/auth/AuthViewModel.kt
@@ -1,14 +1,10 @@
 package com.studhub.app.presentation.auth
 
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.android.gms.auth.api.identity.BeginSignInResult
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.firebase.auth.AuthCredential
 import com.studhub.app.core.utils.ApiResponse
 import com.studhub.app.domain.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,17 +16,8 @@ import javax.inject.Inject
 @HiltViewModel
 class AuthViewModel @Inject constructor(
     private val repo: AuthRepository,
-    val oneTapClient: SignInClient
 ) : ViewModel() {
     val isUserAuthenticated get() = repo.isUserAuthenticatedInFirebase
-
-    val isUserEmailVerified get() = repo.isEmailVerified
-
-    var oneTapSignInResponse by mutableStateOf<ApiResponse<BeginSignInResult>>(ApiResponse.Loading)
-        private set
-
-    var signInWithGoogleResponse by mutableStateOf<ApiResponse<Boolean>>(ApiResponse.Loading)
-        private set
 
     var signInResponse by mutableStateOf<ApiResponse<Boolean>>(ApiResponse.Loading)
         private set
@@ -107,30 +94,6 @@ class AuthViewModel @Inject constructor(
             when (it) {
                 is ApiResponse.Success -> {
                     sendPasswordResetEmailResponse = it
-                }
-                else -> {}
-            }
-        }
-    }
-
-    fun oneTapSignIn() = viewModelScope.launch {
-        oneTapSignInResponse = ApiResponse.Loading
-        repo.oneTapSignInWithGoogle().collect {
-            when (it) {
-                is ApiResponse.Success -> {
-                    oneTapSignInResponse = it
-                }
-                else -> {}
-            }
-        }
-    }
-
-    fun signInWithGoogle(googleCredential: AuthCredential) = viewModelScope.launch {
-        oneTapSignInResponse = ApiResponse.Loading
-        repo.firebaseSignInWithGoogle(googleCredential).collect {
-            when (it) {
-                is ApiResponse.Success -> {
-                    signInWithGoogleResponse = it
                 }
                 else -> {}
             }
