7c1c57efe11a661ed6929b1faad3e7f714953cce
unknown <edouard.michelin15@gmail.com>
2023-05-04 14:39:12 +0200

    Fix wrong top app bar on auth screen

--- app/src/main/java/com/studhub/app/presentation/auth/components/AuthTopBar.kt
+++ app/src/main/java/com/studhub/app/presentation/auth/components/AuthTopBar.kt
@@ -1,11 +1,13 @@
 package com.studhub.app.presentation.auth.components
 
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.studhub.app.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AuthTopBar() {
     TopAppBar(
