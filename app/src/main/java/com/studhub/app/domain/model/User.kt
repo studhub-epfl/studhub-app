515f4be2e6a21a8450faf1f041767dbd05f0f24c
unknown <edouard.michelin15@gmail.com>
2023-05-05 07:08:52 +0200

    Update user model with pictureUri nullable field

--- app/src/main/java/com/studhub/app/domain/model/User.kt
+++ app/src/main/java/com/studhub/app/domain/model/User.kt
@@ -1,5 +1,7 @@
 package com.studhub.app.domain.model
 
+import android.net.Uri
+
 data class User(
     val id: String = "",
     val email: String = "",
@@ -8,6 +10,7 @@ data class User(
     val lastName: String = "",
     val userName: String = "",
     val profilePicture: String = "",
+    val profilePictureUri: Uri? = null,
     val favoriteListings: Map<String, Boolean> = emptyMap(),
     val blockedUsers: Map<String, Boolean> = emptyMap(),
 )
