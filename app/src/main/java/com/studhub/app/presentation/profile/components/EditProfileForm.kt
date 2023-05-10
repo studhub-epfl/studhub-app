08d6576617f3c51888405e6d4607b5693924124f
raynasr <100310446+raynasr@users.noreply.github.com>
2023-05-10 18:00:42 +0200

    Refactoring

--- app/src/main/java/com/studhub/app/presentation/profile/components/EditProfileForm.kt
+++ app/src/main/java/com/studhub/app/presentation/profile/components/EditProfileForm.kt
@@ -61,7 +61,7 @@ fun EditProfileForm(
             }
 
             Spacer(Modifier.size(10.dp))
-            //r
+
             // Disable the button if the phone number is not a valid Swiss number
             Button(
                 onClick = {
