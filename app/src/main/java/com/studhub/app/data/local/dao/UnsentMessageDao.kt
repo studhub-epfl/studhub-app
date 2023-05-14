689556724ffdd58f6123e1a41fcc10e9d76a0040
edouardmichelin <42861545+edouardmichelin@users.noreply.github.com>
2023-05-14 14:09:13 +0200

    Feature/issue 143 sync offline messages (#151)
    Implement synchronization of messages that were sent offline when back online.
    A user can now send messages in different conversations while offline -> these messages will be uploaded/flushed to the online database as soon as internet connection is restored (The user does not need to be on the message page, they can be anywhere on the application as long as they are logged in).
    
    * Add new MainViewModel
    
    * Add @HiltViewModel annotation to MainViewModel
    
    * Add UnsentMessage entity to the local database (cache)
    
    * Remove updateLastMessageWith from conversation repo
    
    * Add 'flushPendingMessages' to message repository
    
    * Reformat code
    
    * Add possibility to turn on/off connection in test network manager
    
    * Fix tests
    
    * Add tests for unsent messages
    
    * Add tests for MessageRepoImpl.flushPendingMessages
    
    * Remove duplicate unbuffer call in flushPendingMessages

---
+++ app/src/main/java/com/studhub/app/data/local/dao/UnsentMessageDao.kt
@@ -0,0 +1,19 @@
+package com.studhub.app.data.local.dao
+
+import androidx.room.*
+import com.studhub.app.data.local.entity.UnsentMessage
+
+@Dao
+interface UnsentMessageDao {
+    @Query("SELECT * FROM unsent_message WHERE conversationId = :conversationId ORDER BY createdAt ASC")
+    suspend fun getUnsentMessages(conversationId: String): List<UnsentMessage>
+
+    @Query("SELECT * FROM unsent_message WHERE senderId = :senderId ORDER BY createdAt ASC")
+    suspend fun getUnsentMessagesOfUser(senderId: String): List<UnsentMessage>
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insertUnsentMessage(unsentMessage: UnsentMessage)
+
+    @Query("DELETE FROM unsent_message WHERE senderId = :senderId")
+    suspend fun deleteUnsentMessages(senderId: String)
+}
