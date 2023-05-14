c2bcb4e44e650642179ef608a8cdd198db9585df
unknown <edouard.michelin15@gmail.com>
2023-05-14 05:55:43 +0200

    Add tests for MessageRepoImpl.flushPendingMessages

--- app/src/androidTest/java/com/studhub/app/data/repository/MessageRepositoryImplTest.kt
+++ app/src/androidTest/java/com/studhub/app/data/repository/MessageRepositoryImplTest.kt
@@ -2,23 +2,25 @@ package com.studhub.app.data.repository
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.studhub.app.core.utils.ApiResponse
+import com.studhub.app.data.local.LocalDataSource
+import com.studhub.app.data.local.database.LocalAppDatabase
 import com.studhub.app.domain.model.Conversation
 import com.studhub.app.domain.model.Message
+import com.studhub.app.domain.model.User
 import com.studhub.app.domain.repository.ConversationRepository
 import com.studhub.app.domain.repository.MessageRepository
 import com.studhub.app.domain.usecase.conversation.SendMessage
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
-import org.junit.Assert.fail
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.*
 import javax.inject.Inject
 import kotlin.random.Random
 
@@ -39,9 +41,111 @@ class MessageRepositoryImplTest {
     @Inject
     lateinit var sendMessage: SendMessage
 
+    @Inject
+    lateinit var localDb: LocalAppDatabase
+
     @Before
     fun init() {
         hiltRule.inject()
+    }
+
+    @Test
+    fun bufferedMessagesAreCorrectlyFlushed() {
+        val cache = LocalDataSource(localDb)
+        val senderId = "Bobby-${Random.nextLong()}"
+        lateinit var conversation: Conversation
+
+        // create fake conversation
+        runBlocking {
+            conversationRepository.createConversation(
+                Conversation(
+                    user1Id = senderId,
+                    user2Id = "Maya"
+                )
+            ).collect {
+                when (it) {
+                    is ApiResponse.Loading -> {}
+                    is ApiResponse.Failure -> fail()
+                    is ApiResponse.Success -> conversation = it.data
+                }
+            }
+        }
+
+        val message1 = Message(
+            conversationId = conversation.id,
+            senderId = senderId,
+            createdAt = Date(1000),
+            content = Random.nextLong().toString()
+        )
+        val message2 = Message(
+            conversationId = conversation.id,
+            senderId = senderId,
+            createdAt = Date(99999),
+            content = Random.nextLong().toString()
+        )
+
+        // buffer messages
+        runBlocking {
+            cache.saveUnsentMessage(message1)
+            cache.saveUnsentMessage(message2)
+        }
+
+        // flush messages
+        runBlocking {
+            messageRepository.flushPendingMessages(User(id = senderId)).collect {
+                when (it) {
+                    is ApiResponse.Failure -> fail("flushPendingMessages failed")
+                    is ApiResponse.Loading -> {}
+                    is ApiResponse.Success -> assertTrue(it.data)
+                }
+            }
+        }
+
+        var messages: List<Message>
+
+        runBlocking { messages = cache.getUnsentMessagesOfUser(senderId) }
+
+        assertEquals("Flushed messages should be removed from the buffer", 0, messages.size)
+
+        // fetch flushed messages from the database
+        runBlocking {
+            launch {
+                messageRepository.getConversationMessages(conversation)
+                    .collect {
+                        when (it) {
+                            is ApiResponse.Success -> {
+                                // check there are only 2 messages
+                                assertEquals(
+                                    "Only 2 messages should have been flushed",
+                                    2,
+                                    it.data.size
+                                )
+
+                                // oldest messages should come first
+                                assertEquals(
+                                    "First message correctly retrieved",
+                                    message1.content,
+                                    it.data.first().content
+                                )
+                                assertEquals(
+                                    "Second message correctly retrieved",
+                                    message2.content,
+                                    it.data.last().content
+                                )
+
+                                cancel()
+                            }
+                            is ApiResponse.Failure -> {
+                                fail("Get Conversation Messages returned failure")
+                                cancel()
+                            }
+                            is ApiResponse.Loading -> {}
+                        }
+                    }
+            }
+        }
+
+        localDb.clearAllTables()
     }
 
     @Test
