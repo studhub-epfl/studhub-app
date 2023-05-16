9838d45c84670866f29f094ed5ec37ca8537913c
tepoorte <samuel.tepoorten@epfl.ch>
2023-05-16 22:51:43 +0200

    tests working on retrieving categories from firebase database, and checking parenting index

--- app/src/main/java/com/studhub/app/data/repository/CategoryRepositoryImpl.kt
+++ app/src/main/java/com/studhub/app/data/repository/CategoryRepositoryImpl.kt
@@ -27,46 +27,6 @@ class CategoryRepositoryImpl @Inject constructor(
     private val storageHelper = StorageHelper()
 
 
-//like a tree structure
-    private val categories: List<Category> = listOf(
-        Category(id = "1", name = "Electronics", description = "phones/cameras/.. ", parentCategoryId = null),
-        Category(id = "2", name = "phone", description = "", parentCategoryId = "1"),
-        Category(id = "3", name = "camera", description = "", parentCategoryId = "1" ),
-
-        Category(id = "4", name = "School Items", description ="books/pencils/bags/..", parentCategoryId = null),
-        Category(id = "5", name = "book", description = "", parentCategoryId = "4"),
-        Category(id = "6", name = "pencils", description = "", parentCategoryId = "4" ),
-        Category(id = "7", name = "bag", description = "", parentCategoryId = "4" ),
-
-        Category(id = "8", name = "Accessories", description = "keys/necklaces/..", parentCategoryId = null),
-        Category(id = "9", name = "key", description = "", parentCategoryId = "8"),
-        Category(id = "10", name = "necklace", description = "", parentCategoryId = "8" ),
-
-        Category(id = "11", name = "Instruments", description = "ear phones/guitar/..", parentCategoryId = null),
-        Category(id = "12", name = "ear phone", description = "", parentCategoryId = "11"),
-        Category(id = "13", name = "guitar", description = "", parentCategoryId = "11" ),
-
-        Category(id = "14", name = "Mobility", description = "bikes/scooter/..", parentCategoryId = null),
-        Category(id = "15", name = "bike", description = "", parentCategoryId = "14"),
-        Category(id = "16", name = "scooter", description = "", parentCategoryId = "14"),
-
-        Category(id = "17", name = "Clothes", description = "pants/shirts/..", parentCategoryId = null),
-        Category(id = "18", name = "pant", description = "", parentCategoryId = "17"),
-        Category(id = "19", name = "shirt", description = "", parentCategoryId = "17"),
-
-        Category(id = "20", name = "Art-decorations", description = "paintings/tapis/..", parentCategoryId = null),
-        Category(id = "21", name = "painting", description = "", parentCategoryId = "20"),
-        Category(id = "22", name = "tapi", description = "", parentCategoryId = "20"),
-
-        Category(id = "23", name = "Services", description = "online services/apps/supports/..", parentCategoryId = null),
-        Category(id = "24", name = "online service", description = "", parentCategoryId = "23"),
-        Category(id = "25", name = "app", description = "", parentCategoryId = "23"),
-        Category(id = "26", name = "support", description = "", parentCategoryId = "23")
-
-    )
-
-
-
     override suspend fun getCategories(): Flow<ApiResponse<List<Category>>> = flow {
         emit(ApiResponse.Loading)
 
