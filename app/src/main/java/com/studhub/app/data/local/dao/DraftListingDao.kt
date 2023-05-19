b2edf4ee01dd9c91a79af6e5f4c2a6f3080220b6
unknown <edouard.michelin15@gmail.com>
2023-05-19 07:01:56 +0200

    Add new local entity and methods for draft listings

---
+++ app/src/main/java/com/studhub/app/data/local/dao/DraftListingDao.kt
@@ -0,0 +1,22 @@
+package com.studhub.app.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.studhub.app.data.local.entity.DraftListing
+
+@Dao
+interface DraftListingDao {
+    @Query("SELECT * FROM draft_listing WHERE sellerId = :sellerId ORDER BY lastModifiedAt DESC")
+    suspend fun getDraftListings(sellerId: String): List<DraftListing>
+
+    @Query("SELECT * FROM draft_listing WHERE draft_listing_id = :listingId")
+    suspend fun getDraftListing(listingId: String): DraftListing
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insertDraftListing(draftListing: DraftListing)
+
+    @Query("DELETE FROM draft_listing WHERE draft_listing_id = :draftListingId")
+    suspend fun deleteDraftListing(draftListingId: String)
+}
