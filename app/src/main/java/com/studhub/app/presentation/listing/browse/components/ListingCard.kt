687a774edaa66fbd35608c0d981a8816bc6deeb9
Edouard <edouard.waked@epfl.ch>
2023-06-01 02:54:45 +0200

    making listing cards (the things on the browsing screen) nicer

--- app/src/main/java/com/studhub/app/presentation/listing/browse/components/ListingCard.kt
+++ app/src/main/java/com/studhub/app/presentation/listing/browse/components/ListingCard.kt
@@ -4,17 +4,13 @@ package com.studhub.app.presentation.listing.browse.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.studhub.app.R
-import com.studhub.app.domain.model.Category
 import com.studhub.app.domain.model.Listing
 import com.studhub.app.domain.model.ListingType
 
@@ -29,49 +25,62 @@ fun ListingCard(listing: Listing, onClick: () -> Unit) {
             .fillMaxWidth(),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.tertiaryContainer
-        )
+        ),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.elevatedCardElevation()
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(90.dp)
-                .clickable { onClick() }
+                .height(120.dp)
+                .clickable { onClick() },
+            verticalAlignment = Alignment.CenterVertically
         ) {
             ThumbnailImage()
-            Box(
+            Column(
                 modifier = Modifier
                     .fillMaxHeight()
-                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp)
             ) {
-                // Item title
                 Text(
-                    modifier = Modifier.padding(start = 5.dp),
                     text = listing.name,
-                    style = MaterialTheme.typography.labelLarge
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(bottom = 8.dp)
                 )
-                // Price chip
-                Column(modifier = Modifier.align(Alignment.BottomStart)) {
+                Row(
+                    verticalAlignment = Alignment.Bottom,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                ) {
                     if (listing.type == ListingType.BIDDING) {
                         Text(
-                            modifier = Modifier.padding(3.dp),
                             text = stringResource(R.string.bidding_type_indicator),
-                            color = MaterialTheme.colorScheme.secondary
+                            style = MaterialTheme.typography.bodySmall,
+                            color = MaterialTheme.colorScheme.secondary,
+                            modifier = Modifier.padding(end = 4.dp)
                         )
                     }
                     PriceChip(
-                        price = listing.price
+                        price = listing.price,
+                        modifier = Modifier.padding(end = 4.dp)
                     )
                 }
-                // Category name and seller
-                CategoryAndSellerInfo(
-                    category = if (listing.categories.isEmpty()) Category() else listing.categories[0],
-                    seller = listing.seller,
-                    modifier = Modifier
-                        .align(Alignment.BottomEnd)
-                        .padding(end = 5.dp, bottom = 4.dp)
-                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Text(
+                        text = "Category: " + (if (listing.categories.isEmpty()) "N/A" else listing.categories[0].name),
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = "Seller: ${listing.seller.firstName} ${listing.seller.lastName}",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                }
             }
         }
     }
-
 }
